fc62c6c601860ee3db1b6f25f3bc39b207c05176
Tucker <icecream@vonholten.name>
2018-01-28 03:42:01 -0500

    Added recyclerview to java

--- app/src/main/java/com/tuckervh/schicken/MainActivity.java
+++ app/src/main/java/com/tuckervh/schicken/MainActivity.java
@@ -4,10 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
     }
 }
