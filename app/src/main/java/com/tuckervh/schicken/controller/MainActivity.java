8c61f19614dca9581b0f21febfe4098a294e3933
william-crawford <will.crawford@live.com>
2018-01-28 10:11:24 -0500

    Added divider and fixed recyclerview spacing

--- app/src/main/java/com/tuckervh/schicken/controller/MainActivity.java
+++ app/src/main/java/com/tuckervh/schicken/controller/MainActivity.java
@@ -2,6 +2,7 @@ package com.tuckervh.schicken.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
@@ -37,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
 
         // specify an adapter (see also next example)
         mAdapter = new ConversationAdapter(conversationList);
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+
         mRecyclerView.setAdapter(mAdapter);
     }
 
